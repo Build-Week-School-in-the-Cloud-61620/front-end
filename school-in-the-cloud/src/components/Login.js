e09617c516af2d574aed2f152aede422d94caa5d
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-06-20 11:21:05 -0700

    added functionality to login form, able to hit redux store with data

--- school-in-the-cloud/src/components/Login.js
+++ school-in-the-cloud/src/components/Login.js
@@ -1,15 +1,61 @@
-import React from "react";
-import { Button, Form, FormGroup, Label, Input, Card } from "reactstrap";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+} from "reactstrap";
+import { loginUser } from "../actions";
 
-function Login() {
+const Login = () => {
+  //React-redux hook used to call dispatch from actions folder...
+  const dispatch = useDispatch();
+
+  //Form state for login
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+    role: "",
+  });
+
+  //Destructured credentials object...
+  const { username, password, role } = credentials;
+
+  //Change handler for form inputs...
+  const handleChanges = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  //Submit handler to access application...
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginUser(credentials));
+    setCredentials({ username: "", password: "", role: "" });
+  };
+  console.log(credentials);
+
+  //Added functionality to form...
   return (
-    <Form style={{ margin: "15%" }}>
+    <Form onSubmit={handleSubmit} style={{ margin: "15%" }}>
       <Card color="info">
         <h2 style={{ color: "whitesmoke", margin: "0 auto" }}>Login Here!</h2>
       </Card>
       <FormGroup>
-        <Label for="email">Email</Label>
-        <Input type="email" name="email" id="email" placeholder="Email Here" />
+        <Label for="username">User Name</Label>
+        <Input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Enter user name"
+          onChange={handleChanges}
+          value={username}
+        />
       </FormGroup>
 
       <FormGroup>
@@ -19,12 +65,29 @@ function Login() {
           name="password"
           id="password"
           placeholder="Password Here"
+          onChange={handleChanges}
+          value={password}
         />
       </FormGroup>
+      {/* Added select component from reactstrap to add role to login form*/}
+      <FormGroup>
+        <Label for="exampleSelect">Select</Label>
+        <Input
+          type="select"
+          name="role"
+          id="exampleSelect"
+          value={role}
+          onChange={handleChanges}
+        >
+          <option>Admin</option>
+          <option>Student</option>
+          <option>Volunteer</option>
+        </Input>
+      </FormGroup>
 
-      <Button>Submit</Button>
+      <Button type="submit">Submit</Button>
     </Form>
   );
-}
+};
 
 export default Login;
