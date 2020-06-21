1b663b99505f98e1821e555f5b69607b6ee3b07e
sarahmarie1976 <sholle7@gmail.com>
2020-06-20 22:59:18 -0500

    Updated Student form and started Volunteer form (including new Task component). Modified authorization method in App.js. Also modified Home component.

--- school-in-the-cloud/src/components/Admin.js
+++ school-in-the-cloud/src/components/Admin.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import { Button, Form, FormGroup, Label, Input, Card } from "reactstrap";
 
-function Admin() {
+function Admin(props) {
+
+  const role = props.role;
+  
   return (
+    <> 
+    <div>
+      {role === "admin" ? 
+
     <Form style={{ margin: "15%" }}>
-      <Card color="info">
-        <h2 style={{ color: "whitesmoke", margin: "0 auto" }}>I'm an Admin!</h2>
-      </Card>
+      <Card color='' style={{background: '#87CEFA'}}>
+      <h2 style={{ color: 'whitesmoke', margin: '0 auto'}}>
+        I'm an Admin!
+      </h2>
+      
+    </Card>
     </Form>
+    : <Redirect to="/" />}
+    </div>
+    </>
   );
 }
 
---
+++ school-in-the-cloud/src/components/Task.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, Form, FormGroup, Label, Input, Card, ListGroupItem } from "reactstrap";
+
+function Task(props) {
+
+  // const  group2  = group;
+  const renderAuthButton = ()=>{
+    if(props.status === "In Progress"){
+      return <Button color="primary">Complete</Button>
+    } else{
+      return <Button disabled>Complete</Button>
+    }
+  }
+
+  return (
+    <tr>
+    <td>{props.task}</td>
+    <td>{props.status}</td>
+    <td>{renderAuthButton()}</td>
+  </tr>
+  );
+}
+
+export default Task;
