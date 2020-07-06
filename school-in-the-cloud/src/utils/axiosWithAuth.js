2728a70373b3c994f058f893d96ecaea41fe7737
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-07-06 15:31:21 -0700

    added heroku link

--- school-in-the-cloud/src/utils/axiosWithAuth.js
+++ school-in-the-cloud/src/utils/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://school-in-the-cloud-ls-bw.herokuapp.com/api",
     headers: {
       authorization: token,
     },
